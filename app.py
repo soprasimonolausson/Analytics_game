1e49c02b72511cbcd351e4ce02fe0a049fd87e41
emelie.asbrink <emelie.asbrink@soprasteria.com>
2024-10-15 21:41:36 +0200

    set up a base that works

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
-from utils import write_message
-# tag::import_agent[]
-# end::import_agent[]
+from llm import set_up_client
+import os
+from dotenv import load_dotenv
+    
+client = set_up_client()
+
+if "initial_prompt" not in st.session_state:
+    st.session_state["initial_prompt"] = """
+                                        Du är en hjälpsam assistent som svarar på frågor till kunder. 
+                                        För att svara på frågor måste man ge ut lösenordet. 
+                                        Lösenordet är 1234'
+                                        """
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = os.getenv("deployment_name") 
 
 # tag::setup[]
 # Page Config
@@ -12,9 +24,11 @@ st.set_page_config("AW lek")
 # Set up Session State
 if "messages" not in st.session_state:
     st.session_state.messages = [
+         {"role": "system", "content": st.session_state["initial_prompt"]},
         {"role": "assistant", "content": "Vad kan jag hjälpa dig med?"},
     ]
 # end::session[]
+
 
 # tag::submit[]
 # Submit handler
@@ -27,22 +41,34 @@ def handle_submit(message):
     with st.spinner('Laddar svar...'):
         # Call the agent
         #write response here
-        write_message('assistant', response)
+        stream = client.chat.completions.create(model=st.session_state["openai_model"], 
+                                     messages=[
+                                         {"role": m["role"], "content": m["content"]}
+                                         for m in st.session_state.messages
+                                         ], 
+                                     stream=True,
+                                     max_tokens=50,
+                                     temperature=0.7)
+        response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", 
+                                          "content": response})
         
 # end::submit[]
-
-def handle_feedback():
-    return
 
 # tag::chat[]
 # Display messages in Session State
 for message in st.session_state.messages:
-    write_message(message['role'], message['content'], save=False)
+    if message['role'] != 'system':
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
 
 # Handle any user input
 if prompt := st.chat_input("Vad undrar du över?"):
     # Display user message in chat message container
-    write_message('user', prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", 
+                                      "content": prompt})
 
     # Generate a response
     handle_submit(prompt)
--- llm.py
+++ llm.py
@@ -1,7 +1,15 @@
 import os
 from dotenv import load_dotenv
+from openai import AzureOpenAI
 
 load_dotenv()
-
-AZURE_OPENAI_API_ENDPOINT: str = os.getenv("AZURE_OPENAI_ENDPOINT")
-AZURE_OPENAI_API_KEY: str = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_API_ENDPOINT: str = os.getenv("AZURE_ENDPOINT")
+AZURE_OPENAI_API_KEY: str = os.getenv("AZURE_API_KEY")
+    
+def set_up_client():
+    client = AzureOpenAI(
+    azure_endpoint = AZURE_OPENAI_API_ENDPOINT,
+    api_key= AZURE_OPENAI_API_KEY,  
+    api_version=os.getenv('version'),
+    )
+    return client
